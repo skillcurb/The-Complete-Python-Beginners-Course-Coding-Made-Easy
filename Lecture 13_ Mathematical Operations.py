7ef45fc0728f07eb4f3cca1e21664f15b8bfbb5d
SkillCurb <66582199+skillcurb@users.noreply.github.com>
2020-06-09 17:38:15 +0500

    Module 4 Code
    This branch contains code in Jupyter Notebook and Python Format of Module 4

---
+++ Lecture 13_ Mathematical Operations.py
@@ -0,0 +1,53 @@
+5 + 2
+
+4 - 3
+
+2 * 9
+
+15 / 2
+
+15 // 2
+
+15 % 2
+
+2 ** 3
+
+6 * 8 / 2 * 2
+
+(2 + 3) * 4 / 2
+
+
+#Relational Operators
+
+num1 = 12
+num2 = 2
+num1 > num2
+
+num1 = 2
+num2 =2 
+num1 >= num2
+
+num1 > num2
+
+num1 == num2
+
+num1 != num2
+
+n1 = 5
+n2 = 9
+n1 < n2
+
+n1 != n2
+
+#Logical Operators
+
+5 > 3
+
+(5 > 3) and (2 < 5)
+
+(5 < 3) and (2 < 5)
+
+(5 < 3) or (2 < 5)
+
+x = True
+print("inverse value of x is",not x)
---
+++ lecture14-Builtin Mathematical Functions.py
@@ -0,0 +1,47 @@
+# to use built-in mathematical functions import library math
+import math
+
+val = math.pi
+val
+
+#calculating area of circle having radius 3
+# area of circle = pi * r^2
+# here using pi connstant power function
+radius = 3
+area = math.pi * math.pow(radius,2)
+area
+
+round(radius,3)
+
+math.floor(area)
+
+math.ceil(area)
+
+math.trunc(area)
+
+math.e
+
+math.exp(3)
+
+math.log(math.pi)
+
+math.log(50)
+
+math.log10(50)
+
+math.sqrt(4)
+
+num = math.radians(90)
+num
+
+math.degrees(num)
+
+angle_Degrees = 60
+angle_Radians = math.radians(angle_Degrees)
+
+print('The value of the angle is:', angle_Radians)
+print('sin(x) is:', math.sin(angle_Radians))
+print('tan(x) is:', math.tan(angle_Radians))
+print('cos(x) is:', math.cos(angle_Radians))
+
+math.factorial(4)
